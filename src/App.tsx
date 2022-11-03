22229faf27cc321b0ac6748537c1a77efc9083fc
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2022-11-03 23:29:16 +0400

    data converting

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,36 @@ import styles from './App.module.scss';
 // mockedData has to be replaced with parsed Promises’ data
 const mockedData: Row[] = rows.data;
 
+const dataConverter = (
+  users: User[],
+  accounts: Account[],
+  images: Image[]
+): Row[] => {
+  let rows: Row[] = users.map(user => {
+    let image = images.find(img => user.userID === img.userID);
+    let account = accounts.find(acc => user.userID === acc.userID);
+    return {
+      username: user.username,
+      country: user.country,
+      name: user.name,
+      avatar: image?.url,
+      posts: account?.posts,
+      lastPayments: account?.payments[0].totalSum,
+    };
+  });
+  return rows;
+};
+
 export const App: FC = () => {
   const [data, setData] = useState<Row[]>(undefined);
 
   useEffect(() => {
     // fetching data from API
-    Promise.all([
-      getImages(),
-      getUsers(),
-      getAccounts(),
-    ]).then(([images, users, accounts]: [Image[], User[], Account[]]) =>
-      console.log(images, users, accounts)
+    Promise.all([getImages(), getUsers(), getAccounts()]).then(
+      ([images, users, accounts]: [Image[], User[], Account[]]) => {
+        const rows = dataConverter(users, accounts, images);
+        setData(rows);
+      }
     );
   }, []);
 
